6ea793f91a74a777b2fce8d0a4ac1a37d92c6482
Salman Ghouri <salmanwasimghouri@gmail.com>
2022-05-03 20:18:17 +0500

    add bounce

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,10 @@ import Whatcandoyet from './components/Whatcandoyet'
 export default function Home() {
   return (
   <>
-<Fade duration='100'>
+<Bounce duration='100'>
 <Header/>
  <Whatcandoyet/>
- </Fade>
- <a className="fixed bottom-0 right-0 flex items-center justify-center h-8 pl-1 pr-2 mb-6 mr-4 text-blue-100 bg-blue-600 rounded-full shadow-lg hover:bg-blue-600" href="/" target="_top"> Join Our Community</a>
+ </Bounce>
   </>
   )
 }
