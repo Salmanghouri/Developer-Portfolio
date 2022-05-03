88a20485e2b5dd49a241e20b06eabc694a85734b
Salman Ghouri <salmanwasimghouri@gmail.com>
2022-05-03 18:27:09 +0500

    add icons once

--- pages/Contact/index.js
+++ pages/Contact/index.js
@@ -29,7 +29,7 @@ const Contact = () => {
             <p className="text-gray-500 text-xl font-bold my-3 max-w-xl leading-relaxed sm:mx-auto lg:ml-0">
               I am available on almost every social media. You can message me, I
               will reply within 24 hours. I can help you with ML, AI, React,
-              Android, Cloud and Opensource Development.{" "}
+              Android, Cloud and Opensource Developments.{" "}
             </p>
             <div>
               <div className="icons text-[48px] cursor-pointer my-6 space-x-3  flex mx-20">
